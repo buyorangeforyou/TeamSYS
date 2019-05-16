0c2e32852979c6a6c2c33cf3f504e9da24a74ba7
chenglong_33 <chenglong_33@outlook.com>
2019-05-16 21:01:48 +0800

    create Login

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+import Login from './views/Login.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -20,6 +22,12 @@ export default new Router({
       component: function () { 
         return import(/* webpackChunkName: "about" */ './views/About.vue')
       }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+
     }
   ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <header>{{user.userName}}{{pass}}</header>
+        <body></body>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+    data () {
+        return {
+            user:{
+                userName: 'lcl',
+
+            },
+            pass: "123456",
+        }
+    },
+    methods:{
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
